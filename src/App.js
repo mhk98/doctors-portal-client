46870e2c8bf343ad8f564bf2a4abb7f4454f6ae6
Md Mohsin Kabir <mohsinkabirseo@gmail.com>
2022-05-11 15:42:26 +0600

    portal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
