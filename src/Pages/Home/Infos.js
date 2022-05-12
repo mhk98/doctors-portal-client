bdd41c5933a91d7cb29baafeccb3f2557c162c34
Md Mohsin Kabir <mohsinkabirseo@gmail.com>
2022-05-12 16:46:17 +0600

    new service

--- src/Pages/Home/Infos.js
+++ src/Pages/Home/Infos.js
@@ -5,7 +5,7 @@ import marker from '../../assets/icons/marker.svg'
 import phone from '../../assets/icons/phone.svg'
 const Infos = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12 text-white'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 text-white'>
             <InforCard cardTitle='Opening Hours' bgClass='bg-gradient-to-r from-secondary to-primary' img={clock}></InforCard>
             <InforCard cardTitle='Our Location' bgClass='bg-accent' img={marker}></InforCard>
             <InforCard cardTitle='Contact Us' bgClass='bg-gradient-to-r from-secondary to-primary' img={phone}></InforCard>
--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -9,9 +9,7 @@ const Service = ({service}) => {
   <div class="card-body items-center text-center">
     <h2 class="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+    
   </div>
 </div>
     );
