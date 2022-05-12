3f597cf1f5369e4f1be03508881b23055e913421
Md Mohsin Kabir <mohsinkabirseo@gmail.com>
2022-05-12 16:07:12 +0600

    add new service section

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Service = ({img}) => {
+const Service = ({service}) => {
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
-    <img src={img}alt="Shoes" class="rounded-xl" />
+    <img src={service.img}alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
     <h2 class="card-title">Shoes!</h2>
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -2,6 +2,7 @@ import React from 'react';
 import fluoride from '../../assets/images/fluoride.png'
 import cavity from '../../assets/images/cavity.png'
 import whitening from '../../assets/images/whitening.png'
+import Service from './Service';
 
 const Services = () => {
     const services = [
@@ -12,13 +13,13 @@ const Services = () => {
             img:fluoride
         },
         {
-            _id: 1,
+            _id: 2,
             name:'Fluoride Treatment',
             description:'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
-            img:fluoride
+            img:cavity
         },
         {
-            _id: 1,
+            _id: 3,
             name:'Fluoride Treatment',
             description:'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
             img:whitening
@@ -30,6 +31,14 @@ const Services = () => {
                 <h3 className='text-secondary font-bold text-xl uppercase'>Our Services</h3>
                 <h2 className='text-3xl'>Services We Provide</h2>
             </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                {
+                    services.map(service =><Service key={service._id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+
         </div>
     );
 };
